0fa354bbb6dcdca9337bee27b26bbebf9ab126e1
metamasao <metamasao@gmail.com>
2023-09-20 01:25:46 +0900

    added utils.py

--- sorted_markdown/build.py
+++ sorted_markdown/build.py
@@ -1,13 +1,28 @@
 import sys
 
+from sort import sorted_markdowns
+
 
 def build():
     print(sys.argv[1])
 
+
 def _build_new():
-    # accept markdown and metadata directory name, using jinja template pass them as parameter, creating config.py
+    # accept markdown and metadata directory name, using jinja template, passing them as parameter, creating config.py
+    pass
+
+
+def _build_category():
+    pass
+
+
+def _build_tags():
+    pass
+
+
+def _build_template_markdown():
     pass
 
 
 if __name__ == "__main__":
-    build()
+    sorted_markdowns("../markdowns")
--- sorted_markdown/sort.py
+++ sorted_markdown/sort.py
@@ -2,20 +2,21 @@ import re
 import os
 from datetime import datetime
 
-from sorted_markdown.exceptions import MetadataNotFoundError
-from sorted_markdown.config import logger, MARKDOWN_DIRECTORY
+from exceptions import MetadataNotFoundError
+from config import logger, MARKDOWN_DIRECTORY
 
 
 def sorted_markdowns(markdown_dir):
     markdown_files = [f"{markdown_dir}/{filename}" for filename in os.listdir(markdown_dir)]
-    return list(map(sorted_markdown, markdown_files))
+    markdown_metadata = list(map(create_markdown_metadata, markdown_files))
+    return sorted(markdown_metadata, key=lambda item: item["datetime"], reverse=True)
 
 
-def sorted_markdown(filename):
+def create_markdown_metadata(filename):
     with open(filename) as f:
         content = f.read()
         check_is_metadata(filename, content)
-        return SortedMetadata.create_sortedmetadata(filename, content)
+        return MarkdownMetadata.create_metadata(content)
 
 
 def check_is_metadata(filename, content):
@@ -24,16 +25,15 @@ def check_is_metadata(filename, content):
     return True
 
 
-class SortedMetadata(dict):
+class MarkdownMetadata(dict):
 
     def __getitem__(self, key):
         item = super().__getitem__(key)
         return datetime.fromisoformat(item) if key == "datetime" else item
 
     @classmethod
-    def create_sortedmetadata(cls, content):
-        content = parse_markdown(content)
-        return sorted(cls(content), key=lambda item: item["datetime"], reverse=True)
+    def create_metadata(cls, content):
+        return cls(parse_markdown(content))
     
 
 def parse_markdown(content):
